ed9702b0703b4b2575bb9566871aaa3804460300
danygzatec <62119447+danygzatec@users.noreply.github.com>
2022-09-01 15:01:22 -0500

    hice file de firebase y puse el api

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDVDQVm2TNcBVVd1vLPxxpOFC85vKbel58",
+  authDomain: "proyectoloyalty-82049.firebaseapp.com",
+  projectId: "proyectoloyalty-82049",
+  storageBucket: "proyectoloyalty-82049.appspot.com",
+  messagingSenderId: "1005365615278",
+  appId: "1:1005365615278:web:7b30bfdceb2cb30b40eb3f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
